306bcca9b536b4094cba5b968efe43584dc9f847
KlaraAB <Kmrazkova@hotmail.com>
2024-06-15 12:08:04 -0600

    Challenge completed

--- js/script.js
+++ js/script.js
@@ -1,2 +1,27 @@
 const topicElement = document.querySelector(".topic");
 const countElement = document.querySelector(".count");
+
+const learning = {};
+
+learning.topic = "JS";
+learning.learningGoals = ["Build JS Programs", "Become a pro in coding", "Get employed in this field", "Be an amazing front-end developer"];
+learning.category = "Front End Development";
+learning.topicImportance = "High";
+
+//console.log(learning);
+
+learning.topic = "JavaScript";
+learning.learningGoals.splice(1,1);
+
+//console.log(learning);
+
+topicElement.innerText = `I'm learning ${learning.topic}!`;
+topicElement.classList.remove("hide");
+
+countElement.innerText = `I have ${learning.learningGoals.length} learning goals.`;
+countElement.classList.remove("hide");
+
+
+
+
+
